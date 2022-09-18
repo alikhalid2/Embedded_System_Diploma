f4f606e256ab0dca0213af877fb1f6de205f2659
alikhalid2 <83635110+alikhalid2@users.noreply.github.com>
2022-09-18 21:44:51 +0200

    Calculate the Power of a Number Using Recursion

--- C/Assignments/CFunctions/EX4.c
+++ C/Assignments/CFunctions/EX4.c
@@ -6,29 +6,29 @@
  */
 #include <stdio.h>
 
-int power (int base , int pow);
+int power (int base , int p);
 
 int main ()
 {
-	int base , pow ;
+	int base , p ;
 	printf("Enter base number: ");
 	fflush(stdin); fflush(stdout);
 	scanf("%d",&base);
 
 	printf("Enter power number (Positive integer): ");
 	fflush(stdin); fflush(stdout);
-	scanf("%d",&pow);
+	scanf("%d",&p);
 
-	printf("%d ^ %d = %d", base , pow , power(base,pow));
+	printf("%d ^ %d = %d", base , p , power(base,p));
 
 	return 0;
 }
 
-int power (int base , int pow )
+int power (int base , int p )
 {
-	if(pow !=0)
+	if(p !=0)
 	{
-		return base*power(base ,pow-1 );
+		return base*power(base ,p-1 );
 	}else {
 		return 1 ;
 	}
