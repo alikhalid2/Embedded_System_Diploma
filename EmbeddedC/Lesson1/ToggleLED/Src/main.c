e43b330c2c4f35d59102354ff1d6cfce7522a7f2
alikhalid2 <83635110+alikhalid2@users.noreply.github.com>
2022-10-07 21:59:14 +0200

    Toggle Led Project code*

---
+++ EmbeddedC/Lesson1/ToggleLED/Src/main.c
@@ -0,0 +1,46 @@
+/**
+ ******************************************************************************
+ * @file           : main.c
+ * @author         : Ali Khalid Swilam
+ * @brief          : Main program body
+ ******************************************************************************
+ * @attention
+ *
+ * <h2><center>&copy; Copyright (c) 2022 STMicroelectronics.
+ * All rights reserved.</center></h2>
+ *
+ * This software component is licensed by ST under BSD 3-Clause license,
+ * the "License"; You may not use this file except in compliance with the
+ * License. You may obtain a copy of the License at:
+ *                        opensource.org/licenses/BSD-3-Clause
+ *
+ ******************************************************************************
+ */
+
+
+#include <stdint.h>
+
+		//Addresses
+#define RCC_BASE   0x40021000
+#define PortA_BASE 0x40010800
+
+
+#define RCC_APB2ENR  *(volatile uint32_t *) (RCC_BASE + 0x18)
+#define GPIO_CRH     *(volatile uint32_t *) (PortA_BASE + 0x18)
+#define GPIO_ODR     *(volatile uint32_t *) (PortA_BASE + 0x18)
+		int main(void)
+{
+	RCC_APB2ENR |= 1<<2;
+	GPIO_CRH    &=0xff0fffff ;
+	GPIO_CRH    |=0x00200000 ;
+
+	while(1)
+	{
+		GPIO_ODR |= 1<<13 ;
+		for (int i=0 ; i<5000 ;i++);
+		GPIO_ODR &= ~(1<<13) ;
+
+
+	}
+		return 0;
+}
